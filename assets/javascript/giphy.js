4348da8c1f34a8b0807c1fff315e3b57a963efbb
Jay Cha <kimchikimchi@gmail.com>
2018-08-13 22:45:04 -0400

    - Supports still-image-animate-image toggling. - Enables adding a new entry via form.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -5,39 +5,43 @@ var animals = [ 'cat', 'dog', 'horse', 'goat', 'rabbit', 'chicken', 'turtle',
 
 // Clean the buttons first not to repeat the entries.
 // Draw buttons with each element in Animal arrays.
-animals.forEach (function(animal) {
-    drawButton(animal);
-});
 
-function drawButton(name) {
-    console.log("drawButton for : " + name);
-    var div = $('<div class="btn-group" role="group" aria-label="First group">');
-    div.html(`<button type="button" class="btn btn-secondary" data-value="${name}">${name}</button>`);
-    $(".btn-toolbar").append(div);
+function drawButtons() {
+    $(".btn-toolbar").empty();
+
+    console.log("drawButton for : " + animals);
+    animals.forEach (function(animal) {
+        var div = $('<div class="btn-group" role="group" aria-label="First group">');
+        div.html(`<button type="button" class="btn btn-secondary" data-value="${animal}">${animal}</button>`);
+        $(".btn-toolbar").append(div);
+    });
 }
 
 function drawGiphyResults(data) {
+    $(".giphy").empty();
 
     data.forEach(function(obj) {
         // Using bootstrap card. http://getbootstrap.com/docs/4.1/components/card/#images
-
         var div = $('<div class="card" style="width: 18rem;">');
-        var img = $('<img class="card-img-top">');
-        img.attr('src', obj.images.original_still.url);
-
+        var img = $('<img class="card-img-top giphy_image">');
+        img.attr({
+            src:  obj.images.original_still.url,
+            'data-still': obj.images.original_still.url,
+            'data-animate': obj.images.original.url,
+            'data-state': "still",
+        });
         var div_p = $('<div class="card-body">');
         div_p.html(`<p class="card-text"> Ratings: ${obj.rating} </p>`);
 
         div.append(img, div_p);
-        $(".btn-toolbar").append(div);
-
+        $(".giphy").append(div);
     });
 }
 
 // When button is clicked, determine what animal it corresponds to
 // then make giphy API calls.  Use event deleation to add callback event
 // to buttons created after page rendering.
-$(".btn-toolbar").on("click", ".btn", function (event){
+$(".btn-toolbar").on("click", ".btn", function(){
 
     var api_key = 'DnKDEbL6X89zDU7delnZ4Avu8a0kURZn';
     /* For the params
@@ -60,15 +64,34 @@ $(".btn-toolbar").on("click", ".btn", function (event){
 });
 
 
-// For each of API async calls, parse through the JSON objects and contruct
-// data structure for images and meta data.
+// Whenver image is clicked, look up the image state and toggle the src
+// between the two choices.
 
+$(".giphy").on("click", ".giphy_image", function() {
+    var state = $(this).attr("data-state");
 
-// For constructed meta data, draw image objects and show corresponding Meta
-// data to the user.
+    if ( state === "still" ) {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
 
 
 // When the user enters a text form, do the following:
 // 1) Append the new entry to list.
 // 2) Append a new button with the new entry text.
 // 3) Make giphy call and draw the returned data object's image and meta data.
+
+$("#addEntrySubmit").on("click", function(event) {
+    event.preventDefault();
+    console.log(`Adding: ${ $("#addEntry").val().trim() }`);
+    animals.push( $("#addEntry").val().trim() );
+    drawButtons();
+});
+
+$(document).ready(function() {
+    drawButtons();
+});
